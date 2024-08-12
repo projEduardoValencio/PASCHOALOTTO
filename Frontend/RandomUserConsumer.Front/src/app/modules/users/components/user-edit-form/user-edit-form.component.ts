72e1234e0a99e98cb03e9a042621e74a3146724e
Eduardo Valencio <eduardo.valencio.santos@gmail.com>
2024-08-12 11:34:00 -0300

    feat: update user confirmation

--- Frontend/RandomUserConsumer.Front/src/app/modules/users/components/user-edit-form/user-edit-form.component.ts
+++ Frontend/RandomUserConsumer.Front/src/app/modules/users/components/user-edit-form/user-edit-form.component.ts
@@ -3,6 +3,7 @@ import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} fr
 import {MatFormField, MatLabel} from "@angular/material/form-field";
 import {MatInput} from "@angular/material/input";
 import {UserService} from "../../../../core/providers/user/user.service";
+import {ToastrService} from "ngx-toastr";
 
 @Component({
   selector: 'paschoalotto-user-edit-form',
@@ -25,7 +26,7 @@ export class UserEditFormComponent implements OnInit {
   isError = false;
   errorMessage = '';
 
-  constructor(private fb: FormBuilder, private userService: UserService) {
+  constructor(private fb: FormBuilder, private userService: UserService, private toastr: ToastrService) {
   }
 
   ngOnInit(): void {
@@ -86,5 +87,21 @@ export class UserEditFormComponent implements OnInit {
 
   onSubmit() {
     console.log(this.userForm.value);
+    this.isLoading = true;
+    this.userService.updateUser(this.userForm.value).subscribe(
+      (response) => {
+        response.birthday = new Date(response.birthday).toISOString().split('T')[0];
+        response.account.registrationDate = new Date(response.account.registrationDate).toISOString().split('T')[0];
+        this.userForm.patchValue(response);
+        this.isLoading = false;
+        this.toastr.success('User updated successfully', 'Update Completed!')
+      },
+      (error) => {
+        this.errorMessage = error.message;
+        this.isLoading = false;
+        this.toastr.error('Error while updating user', 'Error')
+        console.error('Error updating user:', error);
+      }
+    )
   }
 }
